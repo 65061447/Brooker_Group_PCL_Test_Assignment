2ad6c8c254049e081e30f6feb9f4be87614e1600
65061447 <smith.dip@spumail.net>
2026-08-02 23:21:48 +0700

    fixing config

--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,14 @@
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
+
 export default defineConfig({
   tanstackStart: {
-    prerender: {
-      routes: ["/"],
-    },
-    server: { 
-      preset: "github-pages",
-      baseURL: "/Brooker_Group_PCL_Test_Assignment/"
-    },
+    server: {
+      preset: "vercel"
+    }
   },
   vite: {
-    base: "/Brooker_Group_PCL_Test_Assignment/", 
+    base: "/Brooker_Group_PCL_Test_Assignment/"
   }
 });
