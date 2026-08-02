01c0d566d865737bc9f58ab90026dc8d026e2bb8
65061447 <smith.dip@spumail.net>
2026-08-02 22:02:18 +0700

    fix: enable prerendering for index.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,11 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
+    prerender: {
+      routes: ["/"],
+      crawlLinks: true,
+    },
     server: { 
-      entry: "server",
       preset: "github-pages",
       baseURL: "/Brooker_Group_PCL_Test_Assignment/"
     },
