f06230573c7f96c6d163bf960e18862116c281d5
65061447 <smith.dip@spumail.net>
2026-08-02 22:59:10 +0700

    fix the config

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,18 @@
 //     React/TanStack dedupe, error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   tanstackStart: {
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
-    server: {
-      preset: "vercel"
-    }
+    prerender: {
+      routes: ["/"],
+    },
+    server: { 
+      preset: "github-pages",
+      baseURL: "/Brooker_Group_PCL_Test_Assignment/"
+    },
   },
   vite: {
-    base: "/Brooker_Group_PCL_Test_Assignment/"
+    base: "/Brooker_Group_PCL_Test_Assignment/", 
   }
 });
